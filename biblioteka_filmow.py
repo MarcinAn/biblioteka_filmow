18669b9dbbc3e02125e4f2d7af56519811920ac8
Marcin <80240893+MarcinAn@users.noreply.github.com>
2023-08-11 20:59:58 +0200

    Update biblioteka_filmow.py

--- biblioteka_filmow.py
+++ biblioteka_filmow.py
@@ -1,45 +1,26 @@
+from datetime import datetime
 import random
 
 
-class Media:
-    def __init__(self, title, release_year):
+class Movie:
+    def __init__(self, title, release_year, genre, views):
         self.title = title
         self.release_year = release_year
+        self.genre = genre
+        self.views = views
 
     def __str__(self):
         return f"{self.title} ({self.release_year})"
 
 
-class Info:
-    def __init__(self, genre, views):
-        self.genre = genre
-        self.views = views
-
-    def __str__(self):
-        return f"{self.genre} ({self.views})"
-
-
-class Movie(Media, Info):
-    def __init__(self, title, release_year, genre, views):
-        Media.__init__(self, title, release_year)
-        Info.__init__(self, genre, views)
-
-    def __str__(self):
-        return super().__str__()
-
-
-class Series(Media, Info):
+class Series(Movie):
     def __init__(self, title, release_year, genre, season, episode, views):
-        Media.__init__(self, title, release_year)
-        Info.__init__(self, genre, views)
+        Movie.__init__(self, title, release_year, genre, views)
         self.season = season
         self.episode = episode
 
     def __str__(self):
         return f"{self.title} S{self.season}E{self.episode}"
-
-
-media_items = []
 
 
 def add_item(item, media_list):
@@ -48,9 +29,9 @@ def add_item(item, media_list):
 
 def get_movies(media_list):
     movies = []
-    for movie in media_list:
-        if isinstance(movie, Movie):
-            movies.append(movie)
+    for item in media_list:
+        if isinstance(item, Movie) and not isinstance(item, Series):
+            movies.append(item)
     movies = sorted(movies, key=lambda x: x.title)
     return movies
 
@@ -74,32 +55,32 @@ def generate_views(title, media_list):
     views = random.randint(1, 100)
     item = search(title, media_list)
     item.views += views
-    return item
 
 
-def play(title):
-    item = search(title)
+def play(title, media_list):
+    item = search(title, media_list)
     item.views += 1
 
 
 def generate_multiple_views(title, media_list):
-    for _ in range(10):
+    for i in range(10):
         generate_views(title, media_list)
 
 
 def top_titles(num_titles, media_list, content_type=None):
-
-    if content_type == "movie":
-        content = Movie
-    elif content_type == "series":
-        content = Series
-    items = []
-    for item in media_list:
-        if isinstance(item, content):
-            items.append(item)
-    items = sorted(items, key=lambda x: x.views, reverse=True)
-    items = items[:num_titles]
-    return items
+    if content_type == None:
+        items = sorted(media_list, key=lambda x: x.views, reverse=True)
+    else:
+        if content_type == "movie":
+            content = Movie
+        elif content_type == "series":
+            content = Series
+        items = []
+        for item in media_list:
+            if isinstance(item, content):
+                items.append(item)
+        items = sorted(items, key=lambda x: x.views, reverse=True)
+    return items[:num_titles]
 
 
 def create_movie(title, release_year, genre, views):
@@ -110,27 +91,31 @@ def create_series(title, release_year, genre, season, episode, views):
     return Series(title, release_year, genre, season, episode, views)
 
 
-add_item(create_movie("The Shawshank Redemption", 1994, "Drama", 0), media_items)
-add_item(create_movie("Pulp Fiction", 1994, "Crime", 0), media_items)
-add_item(
-    create_series("Stranger Things", 2016, "Science Fiction", 1, 1, 0), media_items
-)
-add_item(create_series("Game of Thrones", 2011, "Fantasy", 3, 5, 0), media_items)
+media_items = []
 
-print("Wyświetl listę filmów")
-for movie in get_movies(media_items):
-    print(movie)
+if __name__ == "__main__":
 
-print("Wyświetl listę seriali")
-for serie in get_series(media_items):
-    print(serie)
+    # Wyświetli na konsoli komunikat Biblioteka filmów.
+    print("Biblioteka filmów")
 
-print("Znajdż tytuł")
-print(search("game of thrones", media_items))
+    # Wypełni bibliotekę treścią.
+    add_item(create_movie("The Shawshank Redemption", 1994, "Drama", 0), media_items)
+    add_item(create_movie("Pulp Fiction", 1994, "Crime", 0), media_items)
+    add_item(create_movie("The Green Mile", 1999, "Drama", 0), media_items)
+    add_item(
+        create_series("Stranger Things", 2016, "Science Fiction", 1, 1, 0), media_items
+    )
+    add_item(create_series("Game of Thrones", 2011, "Fantasy", 3, 5, 0), media_items)
+    add_item(create_series("Narcos", 2015, "Thriller", 1, 3, 0), media_items)
 
-print("Dodaj losową liczbę wyświetleń")
-generate_views("Stranger Things", media_items)
+    # Wygeneruje odtworzenia treści za pomocą funkcji generate_views.
+    for item in media_items:
+        generate_views(item.title, media_items)
 
-print("Wyświetl top 1 serial")
-for series in top_titles(1, media_items, "series"):
-    print(series)
+    # Wyświetli na konsoli komunikat Najpopularniejsze filmy i seriale dnia <data>, gdzie <data> to bieżąca data w formacie DD.MM.RRRR.
+    current_date = datetime.now().strftime("%d.%m.%Y")
+    print(f"Najpopularniejsze filmy i seriale dnia {current_date}")
+
+    # Wyświetli listę top 3 najpopularniejszych tytułów.
+    for item in top_titles(3, media_items):
+        print(item)
